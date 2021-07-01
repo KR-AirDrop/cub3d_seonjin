6a90419693d82ea5a9bafec87387ba19c1ea3891
Seonghyuk Choi <seonchoi@c8r6s1.42seoul.kr>
2021-07-01 20:58:11 +0900

    src_norm2

--- src/memory.c
+++ src/memory.c
@@ -1,108 +1,122 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   memory.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seonchoi <seonchoi@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/01 20:15:02 by seonchoi          #+#    #+#             */
+/*   Updated: 2021/07/01 20:57:50 by seonchoi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../cub3d.h"
 
-int     memory_plus(t_info *info)
+int	memory_plus(t_info *info)
 {
-    int i;
-    int j;
+	int	i[2];
 
-    i = 0;
-    if (!(info->texture = (int**)malloc(sizeof(int*) * 11)))
-        return (-1);
-    while (i < 11)
-    {
-        if (!(info->texture[i] = (int*)malloc(sizeof(int) * (texHeight * texWidth))))
-            return (-1);
-        i++;
-    }
-    i = 0;
-    while (i < 11)
-    {
-        j = 0;
-        while (j < texHeight * texWidth)
-        {
-            info->texture[i][j] = 0;
-            j++;
-        }
-        i++;
-    }
-    return (0);
+	i[0] = 0;
+	info->texture = (int **)malloc(sizeof(int *) * 11);
+	if (!info->texture)
+		return (-1);
+	while (i[0] < 11)
+	{
+		info->texture[i[0]] = (int *)malloc(sizeof(int) * (Tex_H * Tex_W));
+		if (!info->texture[i[0]])
+			return (-1);
+		i[0]++;
+	}
+	i[0] = 0;
+	while (i[0] < 11)
+	{
+		i[1] = 0;
+		while (i[1] < Tex_H * Tex_W)
+		{
+			info->texture[i[0]][i[1]] = 0;
+			i[1]++;
+		}
+		i[0]++;
+	}
+	return (0);
 }
 
-int     memory_plus2(t_info *info)
+int	memory_plus2(t_info *info)
 {
-    int i;
-    int j;
+	int	i[2];
 
-    i = 0;
-    if (!(info->path = (char**)malloc(sizeof(char*) * 4)))
-        return (-1);
-    while (i < 4)
-    {
-        if (!(info->path[i] = (char*)malloc(sizeof(char) * (texHeight * texWidth))))
-            return (-1);
-        i++;
-    }
-    i = 0;
-    while (i < 4)
-    {
-        j = 0;
-        while (j < texHeight * texWidth)
-        {
-            info->path[i][j] = 0;
-            j++;
-        }
-        i++;
-    }
-    return (0);
+	i[0] = 0;
+	info->path = (char **)malloc(sizeof(char *) * 4);
+	if (!info->path)
+		return (-1);
+	while (i[0] < 4)
+	{
+		info->path[i[0]] = (char *)malloc(sizeof(char) * (Tex_H * Tex_W));
+		if (!info->path[i[0]])
+			return (-1);
+		i[0]++;
+	}
+	i[0] = 0;
+	while (i[0] < 4)
+	{
+		i[1] = 0;
+		while (i[1] < Tex_H * Tex_W)
+		{
+			info->path[i[0]][i[1]] = 0;
+			i[1]++;
+		}
+		i[0]++;
+	}
+	return (0);
 }
 
-void    initialization(t_info *info)
+void	initialization(t_info *info)
 {
-    info->key.key_a = 0;
+	info->key.key_a = 0;
 	info->key.key_w = 0;
 	info->key.key_s = 0;
 	info->key.key_d = 0;
-    info->key.key_left = 0;
-    info->key.key_right = 0;
+	info->key.key_left = 0;
+	info->key.key_right = 0;
 	info->key.key_esc = 0;
-    info->movespeed = 0.05;
+	info->movespeed = 0.05;
 	info->rotspeed = 0.05;
-    info->zbuffer = (double*)malloc(sizeof(double) * width);
-    info->buf = (int**)malloc(sizeof(int*) * height);
+	info->zbuffer = (double *)malloc(sizeof(double) * width);
+	info->buf = (int **)malloc(sizeof(int *) * height);
 }
 
-void    first_info(t_info *info)
+void	first_info(t_info *info)
 {
-    int i;
-    int j;
+	int	i;
+	int	j;
 
-    initialization(info);
-    i = 0;
-    while (i < height)
-    {
-        info->buf[i] = (int*)malloc(sizeof(int) * (height));
-        i++;
-    }
-    i = 0;
-    while (i < height)
-    {
-        j = 0;
-        while (j < width)
-        {
-            info->buf[i][j] = 0;
-            j++;
-        }
-        i++;
-    }
+	initialization(info);
+	i = 0;
+	while (i < height)
+	{
+		info->buf[i] = (int *)malloc(sizeof(int) * (height));
+		i++;
+	}
+	i = 0;
+	while (i < height)
+	{
+		j = 0;
+		while (j < width)
+		{
+			info->buf[i][j] = 0;
+			j++;
+		}
+		i++;
+	}
 }
 
-void clean_img(t_img *img)
+void	clean_img(t_img *img)
 {
-    img->img = 0;
-    img->data = 0;
-    img->img_height = 0;
-    img->bpp = 0;
-    img->size_l = 0;
-    img->endian = 0;
-    img->img_width = 0;
+	img->img = 0;
+	img->data = 0;
+	img->img_height = 0;
+	img->bpp = 0;
+	img->size_l = 0;
+	img->endian = 0;
+	img->img_width = 0;
 }
--- src/raycasting.c
+++ src/raycasting.c
@@ -2,8 +2,8 @@
 
 void	calc(t_info *info)
 {
-	int	x;
-    t_calac_data data;
+	int				x;
+	t_calac_data	data;
 
 	x = 0;
 	while (x < width)
@@ -16,99 +16,97 @@ void	calc(t_info *info)
 		data.deltadist_x = fabs(1 / data.raydir_x);
 		data.deltadist_y = fabs(1 / data.raydir_y);
 		data.hit = 0;
-        ray_plus(&data, info);
-        hit_check(&data, info);
-        textured_input(&data, info);
-        calc_color(info, &data, x);
-        x++;
-    }
-
-
-}
-
-void    calc_color(t_info *info, t_calac_data *data,int x)
-{
-    int y;
-
-    if (data->side == 0 || data->side == 1)
-		data->wall_x = info->pos_y + data->perpwalldist * data->raydir_y;
-	else
-		data->wall_x = info->pos_x + data->perpwalldist * data->raydir_x;
-	data->wall_x -= floor(data->wall_x);
-    data->tex_x = (int)(data->wall_x * (double)texWidth);
-	if (data->side == 0 && data->raydir_x > 0)
-		data->tex_x = texWidth - data->tex_x - 1;
-	if (data->side == 1 && data->raydir_y < 0)
-		data->tex_x = texWidth - data->tex_x - 1;
-	data->step = 1.0 * texHeight / data->lineheight;
-	data->texPos = (data->drawstart - height / 2 + data->lineheight / 2) * data->step;
-    y = data->drawstart;
-	while (y < data->drawend)	
-	{
-        data->tex_y = (int)data->texPos & (texHeight - 1);
-		data->texPos += data->step;
-		data->color = info->texture[data->texnum][texHeight * data->tex_y + data->tex_x];
-        if (data->side == 0 || data->side == 1 || data->side == 2|| data->side == 3)
-			data->color = info->texture[data->texnum][texHeight * data->tex_y + data->tex_x];
-		info->buf[y][x] = data->color;
-        y++;
+		ray_plus(&data, info);
+		hit_check(&data, info);
+		textured_input(&data, info);
+		calc_color(info, &data, x);
+		x++;
 	}
-    info->zbuffer[x] = data->perpwalldist;
 }
 
-void    hit_check(t_calac_data *data, t_info *info)
+void	calc_color(t_info *info, t_calac_data *d, int x)
 {
-    while (data->hit == 0)
+	if (d->side == 0 || d->side == 1)
+		d->wall_x = info->pos_y + d->perpwalldist * d->raydir_y;
+	else
+		d->wall_x = info->pos_x + d->perpwalldist * d->raydir_x;
+	d->wall_x -= floor(d->wall_x);
+	d->tex_x = (int)(d->wall_x * (double)Tex_W);
+	if (d->side == 0 && d->raydir_x > 0)
+		d->tex_x = Tex_W - d->tex_x - 1;
+	if (d->side == 1 && d->raydir_y < 0)
+		d->tex_x = Tex_W - d->tex_x - 1;
+	d->step = 1.0 * Tex_H / d->lineheight;
+	d->texPos = (d->drawstart - height / 2 + d->lineheight / 2) * d->step;
+	d->index_y = d->drawstart;
+	while (d->index_y < d->drawend)
+	{
+		d->tex_y = (int)d->texPos & (Tex_H - 1);
+		d->texPos += d->step;
+		d->color = info->texture[d->texnum][Tex_H * d->tex_y + d->tex_x];
+		if (d->side == 0 || d->side == 1 || d->side == 2 || d->side == 3)
+			d->color = info->texture[d->texnum][Tex_H * d->tex_y + d->tex_x];
+		info->buf[d->index_y][x] = d->color;
+		d->index_y++;
+	}
+	info->zbuffer[x] = d->perpwalldist;
+}
+
+void	hit_check(t_calac_data *data, t_info *info)
+{
+	while (data->hit == 0)
 	{
 		if (data->sidedist_x < data->sidedist_y)
 		{
 			data->sidedist_x += data->deltadist_x;
 			data->map_x += data->step_x;
-            if (data->step_x == 1)
-                data->side = 0;
+			if (data->step_x == 1)
+				data->side = 0;
 			else if (data->step_x == -1)
-                data->side = 1;
-        }
+				data->side = 1;
+		}
 		else
 		{
 			data->sidedist_y += data->deltadist_y;
 			data->map_y += data->step_y;
 			if (data->step_y == 1)
-                data->side = 2;
-            else if (data->step_y == -1)
-                data->side = 3;
+				data->side = 2;
+			else if (data->step_y == -1)
+				data->side = 3;
 		}
 		if (info->map[data->map_x][data->map_y] == '1')
-            data->hit = 1;
+			data->hit = 1;
 	}
 }
 
-void    textured_input(t_calac_data *data, t_info *info)
+void	textured_input(t_calac_data *data, t_info *info)
 {
-    if (data->side == 0 || data->side == 1)
-		data->perpwalldist = (data->map_x - info->pos_x + (1 - data->step_x) / 2) / data->raydir_x;
+	if (data->side == 0 || data->side == 1)
+		data->perpwalldist = (data->map_x - info->pos_x
+				+ (1 - data->step_x) / 2) / data->raydir_x;
 	else
-		data->perpwalldist = (data->map_y - info->pos_y + (1 - data->step_y) / 2) / data->raydir_y;
+		data->perpwalldist = (data->map_y - info->pos_y
+				+ (1 - data->step_y) / 2) / data->raydir_y;
 	data->lineheight = (int)(height / data->perpwalldist);
 	data->drawstart = -data->lineheight / 2 + height / 2;
-	if(data->drawstart < 0)
+	if (data->drawstart < 0)
 		data->drawstart = 0;
 	data->drawend = data->lineheight / 2 + height / 2;
 	if (data->drawend >= height)
 		data->drawend = height - 1;
-    if (info->map[data->map_x][data->map_y] == '1' && data->side == 0)
-       data->texnum = 0;
-    else if (info->map[data->map_x][data->map_y] == '1' && data->side == 1)
-        data->texnum = 1;
-    else if (info->map[data->map_x][data->map_y] == '1' && data->side == 2)
-        data->texnum = 2;
-    else if (info->map[data->map_x][data->map_y] == '1' && data->side == 3)
-        data->texnum = 3;
+	if (info->map[data->map_x][data->map_y] == '1' && data->side == 0)
+		data->texnum = 0;
+	else if (info->map[data->map_x][data->map_y] == '1' && data->side == 1)
+		data->texnum = 1;
+	else if (info->map[data->map_x][data->map_y] == '1' && data->side == 2)
+		data->texnum = 2;
+	else if (info->map[data->map_x][data->map_y] == '1' && data->side == 3)
+		data->texnum = 3;
 }
 
-void    ray_plus(t_calac_data *data, t_info *info)
+void	ray_plus(t_calac_data *data, t_info *info)
 {
-    if (data->raydir_x < 0)
+	if (data->raydir_x < 0)
 	{
 		data->step_x = -1;
 		data->sidedist_x = (info->pos_x - data->map_x) * data->deltadist_x;
@@ -116,7 +114,8 @@ void    ray_plus(t_calac_data *data, t_info *info)
 	else
 	{
 		data->step_x = 1;
-		data->sidedist_x = (data->map_x + 1.0 - info->pos_x) * data->deltadist_x;
+		data->sidedist_x = (data->map_x + 1.0 - info->pos_x)
+			* data->deltadist_x;
 	}
 	if (data->raydir_y < 0)
 	{
@@ -126,6 +125,7 @@ void    ray_plus(t_calac_data *data, t_info *info)
 	else
 	{
 		data->step_y = 1;
-		data->sidedist_y = (data->map_y + 1.0 - info->pos_y) * data->deltadist_y;
+		data->sidedist_y = (data->map_y + 1.0 - info->pos_y)
+			* data->deltadist_y;
 	}
 }
